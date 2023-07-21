4925cf9aeeba69ec8af70777c8dc674dd0ec19b0
Krishnendu Dalui <krishnendudalui@gmail.com>
2023-07-21 08:51:30 +0530

    experiment

---
+++ application/controllers/Home.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home  extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper(['form']);
+        $this->load->helper(['url']);
+        
+    }
+
+    public function index()
+    {
+        //echo " Welcome to Gurudev worlds !";
+       $this->load->view('admin_login');
+      
+    }
+}
+?>
---
+++ application/views/admin_login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?= base_url('tool/css/bootstrap.css')?>">
+    <script src="<?= base_url('tool/js/jquery-3.7.0.min.js')?>"></script>
+    <script src="<?= base_url('tool/js/bootstrap.js')?>"></script>
+    <link rel="stylesheet" href="<?= base_url('tool/css/style.css')?>">
+</head>
+<body>
+    <h1>JAY GURUDEV</h1>
+<div class="container-fluid">
+    <div class="container">
+        <div class="col-sm-4"><h2>EKAM ROOM BOOKING SYSTEM </h2></div>
+        <div class="col-sm-8">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Gallery</a></li>
+                <li><a href="#">Restront </a></li>
+                <li><a href="#">Room </a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+</body>
+</html>
